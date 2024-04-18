50887a9fc66056dfd1f9f9476d6c63e591f78b27
AbhayMishra <grabhaymishra@gmail.com>
2024-04-19 02:18:24 +0530

    added enviornment for production

--- app/dashboard/support/page.tsx
+++ app/dashboard/support/page.tsx
@@ -15,106 +15,97 @@ const featureItems = [
   { name: "Unlimited Storage" },
 ];
 
-async function getData(userId :string){
-    try{
+async function getData(userId: string) {
+  try {
     const data = await prisma.subscription.findUnique({
-        where: {
-          userId: userId,
-        },
-        select: {
-          status: true,
-          user: {
-            select: {
-              stripeCustomerId: true,
-            },
+      where: {
+        userId: userId,
+      },
+      select: {
+        status: true,
+        user: {
+          select: {
+            stripeCustomerId: true,
           },
         },
-      });
+      },
+    });
 
-    return data
-
-    }
-    catch (error) {
-        console.log(error)
-    }
-    
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+const Page = async () => {
+  const { userId } = auth();
+  const data = await getData(userId as string);
 
+  async function createSubscription() {
+    "use server";
 
-const Page = async() => {
+    console.log("subscriptionn created");
+    const dbUser = await prisma.user.findUnique({
+      where: {
+        id: userId as string,
+      },
+      select: {
+        stripeCustomerId: true,
+      },
+    });
 
-
-    const {userId} = auth()
-    const data = await getData(userId as string)
-
-    async function createSubscription() {
-        "use server"
-
-         console.log("subscriptionn created")
-        const dbUser =  await prisma.user.findUnique({
-            where:{
-                id:userId as string
-            },
-            select:{
-                stripeCustomerId:true
-            }
-        })
-
-
-        if(!dbUser?.stripeCustomerId){
-            throw new Error ("unable to get customer id")
-        }
-
-        console.log(dbUser.stripeCustomerId)
-        const subscriptionUrl  = await getStripeSession({
-            customerId:dbUser.stripeCustomerId,
-            domainUrl: 'http://localhost:3000',
-            priceId: process.env.STRIPE_PRICE_ID as string
-        })
-
-      
-
-        console.log(subscriptionUrl)
-        return redirect(subscriptionUrl)
+    if (!dbUser?.stripeCustomerId) {
+      throw new Error("unable to get customer id");
     }
 
+    console.log(dbUser.stripeCustomerId);
+    const subscriptionUrl = await getStripeSession({
+      customerId: dbUser.stripeCustomerId,
+      domainUrl:
+        process.env.NODE_ENV === "production"
+          ? (process.env.PRODUCTION_URL as string)
+          : "http://localhost:3000",
+      priceId: process.env.STRIPE_PRICE_ID as string,
+    });
+
+    return redirect(subscriptionUrl);
+  }
 
   return (
     <div className="max-w-md mx-auto space-y-4">
-    <Card className="flex flex-col">
-      <CardContent className="py-8">
-        <div>
-          <h3 className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-primary/10 text-primary">
-            Monthly
-          </h3>
-        </div>
+      <Card className="flex flex-col">
+        <CardContent className="py-8">
+          <div>
+            <h3 className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-primary/10 text-primary">
+              Monthly
+            </h3>
+          </div>
 
-        <div className="mt-4 flex items-baseline text-6xl font-extrabold">
-          $5 <span className="ml-1 text-2xl text-muted-foreground">/mo</span>
-        </div>
-        <p className="mt-5 text-lg text-muted-foreground">
-          Support us just only for 5$ monthly
-        </p>
-      </CardContent>
-      <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:p-10 sm:pt-6">
-        <ul className="space-y-4">
-          {featureItems.map((item, index) => (
-            <li key={index} className="flex items-center">
-              <div className="flex-shrink-0">
-                <CheckCircle2 className="h-6 w-6 text-green-500" />
-              </div>
-              <p className="ml-3 text-base">{item.name}</p>
-            </li>
-          ))}
-        </ul>
+          <div className="mt-4 flex items-baseline text-6xl font-extrabold">
+            $5 <span className="ml-1 text-2xl text-muted-foreground">/mo</span>
+          </div>
+          <p className="mt-5 text-lg text-muted-foreground">
+            Support us just only for 5$ monthly
+          </p>
+        </CardContent>
+        <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:p-10 sm:pt-6">
+          <ul className="space-y-4">
+            {featureItems.map((item, index) => (
+              <li key={index} className="flex items-center">
+                <div className="flex-shrink-0">
+                  <CheckCircle2 className="h-6 w-6 text-green-500" />
+                </div>
+                <p className="ml-3 text-base">{item.name}</p>
+              </li>
+            ))}
+          </ul>
 
-        <form className="w-full" action={createSubscription}>
-          <StripeSubscriptionCreationButton />
-        </form>
-      </div>
-    </Card>
-  </div>
+          <form className="w-full" action={createSubscription}>
+            <StripeSubscriptionCreationButton />
+          </form>
+        </div>
+      </Card>
+    </div>
   );
 };
 
